7d771313becf705d455194579a7e3b6d4d4c060b
KaiSpellman <kai.spellman03@gmail.com>
2024-03-16 21:49:40 -0700

    added unit tests for sortPoints function

---
+++ src/lib/generateChartImg.test.js
@@ -0,0 +1,1 @@
+const generateChartingImg = require("./generateChartingImg");
---
+++ src/lib/sortPoints.test.js
@@ -0,0 +1,39 @@
+const sortPoints = require("./sortPoints");
+
+describe('sortPoints', () => {
+    test('sort points by ascending X value', () => {
+        const points = [
+            { x: 3, y: 5 },
+            { x: 1, y: 2 },
+            { x: 4, y: 7 },
+        ];
+        const sortedPoints = sortPoints(points);
+        expect(sortedPoints).toEqual([
+            { x: 1, y: 2 },
+            { x: 3, y: 5 },
+            { x: 4, y: 7 },
+        ]);
+    });
+
+    test('do not sort points with non-numeric X values', () => {
+        const points = [
+            { x: 'foo', y: 5 },
+            { x: 1, y: 2 },
+            { x: 'bar', y: 7 },
+        ];
+        const sortedPoints = sortPoints(points);
+        expect(sortedPoints).toEqual(points);
+    });
+
+    test('handle empty array', () => {
+        const points = [];
+        const sortedPoints = sortPoints(points);
+        expect(sortedPoints).toEqual([]);
+    });
+
+    test('handle single-element array', () => {
+        const points = [{ x: 1, y: 2 }];
+        const sortedPoints = sortPoints(points);
+        expect(sortedPoints).toEqual([{ x: 1, y: 2 }]);
+    });
+});
